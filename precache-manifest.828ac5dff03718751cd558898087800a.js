9fb5d95b44ade5618e3c60562b941dbc054aacaf
Rrezon Istrefaj <44949897+Xonzhuan@users.noreply.github.com>
2022-03-26 14:59:07 +0100

    Updates

---
+++ precache-manifest.828ac5dff03718751cd558898087800a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d09e8ba5020a5c04a9a431b95f2ea32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16ab58ae0033406d426d",
+    "url": "/static/css/2.f551b7af.chunk.css"
+  },
+  {
+    "revision": "157f5db2f07ef1a9fb57",
+    "url": "/static/css/main.83cc77da.chunk.css"
+  },
+  {
+    "revision": "16ab58ae0033406d426d",
+    "url": "/static/js/2.3ed80224.chunk.js"
+  },
+  {
+    "revision": "7eef2c8ed9c81212142c39c9bc34ff76",
+    "url": "/static/js/2.3ed80224.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "157f5db2f07ef1a9fb57",
+    "url": "/static/js/main.dc0bcbce.chunk.js"
+  },
+  {
+    "revision": "b5b936ff77497917b09e",
+    "url": "/static/js/runtime-main.625e1d07.js"
+  },
+  {
+    "revision": "0d6c5f3efbdb651afa1d8d6a6974361d",
+    "url": "/static/media/ab.0d6c5f3e.jpeg"
+  }
+]);
